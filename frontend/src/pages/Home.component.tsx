6895268a45880ebeb24a497e6dc084c264a991d2
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-05-12 16:48:33 +0200

    figata fatta

--- frontend/src/pages/Home.component.tsx
+++ frontend/src/pages/Home.component.tsx
@@ -1,18 +1,41 @@
+import { useEffect, useState } from "react";
 import HomeBody from "../components/HomeBody/HomeBody.component";
 import Jumbotron from "../components/Jumbotron/Jumbotron.component";
 import { useAppSelector } from "../redux/store/store";
+import { Col, Row } from "react-bootstrap";
+import logo from "../assets/blackLogoNoBg.png";
+import style from "./Home.module.css";
 // import { useEffect } from "react";
 
 const Home = () => {
   const reduxToken = useAppSelector((state) => state.authToken?.token);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1500);
+  });
 
   return (
     <div>
       <>
+        {loading && (
+          <div className={style.generalContainerOption}>
+            <Row className="w-100" style={{ height: "100%" }}>
+              <Col
+                className="w-100 d-flex justify-content-center align-items-center"
+                style={{ height: "100%" }}
+              >
+                <div className={`${style.logoContainerOptions}`}>
+                  <img className={style.imgOptions} src={logo} />
+                </div>
+              </Col>
+            </Row>
+          </div>
+        )}
         {!reduxToken && (
           <>
             <Jumbotron />
-            <HomeBody />
+            {!loading && <HomeBody />}
           </>
         )}
       </>
