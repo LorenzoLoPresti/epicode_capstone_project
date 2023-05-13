d58818fdc451c1402d3297a9c142c0ae677e3bb9
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-05-13 06:53:42 +0200

    redux-persist installed

--- frontend/src/redux/store/store.ts
+++ frontend/src/redux/store/store.ts
@@ -1,15 +1,26 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { tokenStore } from "../reducers/tokenStore";
 import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import persistStore from "redux-persist/es/persistStore";
 
 const rootReducer = combineReducers({
   authToken: tokenStore.reducer,
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
 
+export const persistor = persistStore(store);
 // export type RootState = ReturnType<typeof rootReducer>;
 export const useAppSelector: TypedUseSelectorHook<
   ReturnType<typeof store.getState>
