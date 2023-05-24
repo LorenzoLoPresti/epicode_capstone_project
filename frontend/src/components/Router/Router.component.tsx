996f33ee4b5f4644f45ad6684b4bc0ab6174d85f
LorenzoLoPresti <lorenzo.lopresti.smm@gmail.com>
2023-05-24 07:16:27 +0200

    404 done

--- frontend/src/components/Router/Router.component.tsx
+++ frontend/src/components/Router/Router.component.tsx
@@ -4,13 +4,14 @@ import Login from "../../pages/Login.component";
 import ChefPage from "../../pages/Negozio/Negozio.component";
 import Shop from "../Shop/Shop.component";
 import Checkout from "../../pages/Checkout/Checkout.component";
+import NotFound from "../../pages/404/NotFound.component";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="*" element={"ErrorPage"} />
+      <Route path="*" element={<NotFound />} />
       <Route path="/store/:nomeRistorante" element={<ChefPage />} />
       <Route path="/shop/:nomeRistorante" element={<Shop />} />
       <Route path="/checkout" element={<Checkout />} />
---
+++ frontend/src/pages/404/NotFound.component.tsx
@@ -0,0 +1,58 @@
+// import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import styles from "./NotFound.module.css";
+import notFound from "../../assets/notFoundImg.jpg";
+import { AiFillQuestionCircle } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div
+      //   className={styles.bg}
+      style={{
+        minHeight: "100vh",
+        backgroundImage: `url(${notFound})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className={`${styles.containerOptions}`}>
+        <Container className="w-100 h-100 pt-5 px-xs-1 px-md-5">
+          <Row
+            className="d-flex justify-content-center align-items-center text-light h-100 w-100"
+            style={{ padding: "5rem 0", margin: "0", minHeight: "100vh" }}
+          >
+            <Col
+              className={`d-flex flex-column align-items-center justify-content-center text-center ${styles.modal}`}
+            >
+              <div className="d-flex align-items-center">
+                <h1 className={`${styles.notFound}`}>404 </h1>
+                <AiFillQuestionCircle
+                  className={`${styles.notFoundIcon} mb-2`}
+                />
+              </div>
+              <div className="mb-3 fs-5 px-md-4">
+                <p>
+                  Siamo spiacenti ma la pagina cercata non esiste o è stata
+                  cancellata
+                </p>
+              </div>
+              <div className="">
+                <p>
+                  <Link to={"/login"} className={`${styles.link} me-1`}>
+                    Accedi
+                  </Link>
+                  o torna alla
+                  <Link to={"/"} className={`${styles.link} ms-1`}>
+                    Homepage
+                  </Link>
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
